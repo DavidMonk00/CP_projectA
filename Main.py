29e2e0187283377295b9811a2bc490ca0e2b6455
David Monk <d.g.monk0@gmail.com>
2016-10-28 11:15:08 +0100

    Further streamlined plotting class

--- Main.py
+++ Main.py
@@ -11,13 +11,12 @@ import Plot
 A = 0.5
 D = 0
 start = np.array([A,0.0])
-pt = Plot.Plot(D,start)
-h = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+pt = Plot.Plot(D,start,2)
 
-i = 0.001
-#for i in h:
-#pt.plotMethod(sa.SmallAngle.leapfrogMethod,i,int(np.pi*2/i))
-#pt.plotMethod(sa.SmallAngle.eulerForwardMethod,i,int(np.pi*2/i))
-#pt.analytical(i,int(np.pi*2/i),A)
-pt.error(sa.SmallAngle.rk4Method, i,int(np.pi*100/i))
+h = 0.1
+steps = int(np.pi*10/h)
+method = sa.SmallAngle.implicitEulerMethod
+pt.plotMethod(method,h,steps,True)
+pt.error(method, h,steps)
+
 pt.show()
--- Plot.py
+++ Plot.py
@@ -9,27 +9,45 @@ import matplotlib.pyplot as plt
 import SmallAngle
 
 class Plot(object):
-	def __init__(self, damping_coefficient, start):
+	def __init__(self, damping_coefficient, start, rows):
 		self.sa = SmallAngle.SmallAngle(damping_coefficient, start)
+		self.fig = plt.figure()
+		self.row_counter = 1
+		self.rows = rows
+		self.subplots = []
+		self.A = start[0]
 
-	def analytical(self, h, steps, A):
+	def analytical(self, h, steps):
 		x = np.arange(0,h*steps,h)
-		y = A*np.cos(x)
-		plt.plot(x,y)
+		y = self.A*np.cos(x)
+		self.subplots[self.row_counter-1].plot(x,y)
+
 	def error(self, method, h, steps):
 		x = np.arange(0,h*steps,h)
 		y = self.sa.error(method, h, steps)
-		plt.plot(x[1:],y)
+		self.subplots.append(self.fig.add_subplot(self.rows,1,self.row_counter))
+		self.subplots[self.row_counter-1].set_ylabel('Error')
+		self.subplots[self.row_counter-1].plot(x[1:],y)
+		self.row_counter += 1
+
 	def stability(self, h, steps):
 		x = np.arange(0,h*steps,h)
 		y = self.sa.stability(h, steps)
-		plt.plot(x[1:],y)
+		ax = self.fig.add_subplot(self.rows,1,self.row_counter)
+		self.row_counter += 1
+		ax.plot(x[1:],y)
 
-	def plotMethod(self, method, h, steps):
+	def plotMethod(self, method, h, steps, true_value=False):
 		x = np.arange(0,h*steps,h)
 		y = method(self.sa,h, steps)
-		plt.plot(x,y)
+		self.subplots.append(self.fig.add_subplot(self.rows,1,self.row_counter))
+		self.subplots[self.row_counter-1].set_ylabel('Value')
+		self.subplots[self.row_counter-1].plot(x,y)
+		if true_value:
+			self.analytical(h,steps)
+		self.row_counter += 1
 
 	def show(self):
+		plt.ticklabel_format(axis='y', style='sci')
 		#plt.ylim([-1,5])
 		plt.show()
