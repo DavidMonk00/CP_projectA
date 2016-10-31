e0c095077000191979379e73d06ddb338d00fe96
David Monk <d.g.monk0@gmail.com>
2016-10-31 00:12:27 +0000

    Updated git repository.

--- smallangle.c
+++ smallangle.c
@@ -74,19 +74,21 @@ double* rk4(double* start, double D, int steps, double h) {
        exit(1);
    }
 
-   double identity[] = {1.0,0.0,0.0,1.0};
    double function[] = {0.0,1.0,-1.0,-D};
    double* matrix = create1DArray(4);
-   for (int i = 0; i < 4; i++) {
-      matrix[i] = identity[i] + h*function[i];
-   }
-
    values[0][0] = start[0];
    values[1][0] = start[1];
+
    for (int i = 1; i < steps; i++){
       double* prev = create1DArray(2);
       prev[0] = values[0][i-1];
       prev[1] = values[1][i-1];
+      double** k = create2DArray(2,4);
+      double* x_i = vectorMatrixOp(matrix,prev);
+      k[0][0] = h*x_i[0];
+      k[1][0] = h*x_i[1];
+
+
       double* x = vectorMatrixOp(matrix,prev);
       values[0][i] = x[0];
       values[1][i] = x[1];
