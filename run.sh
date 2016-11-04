dc8a83af0163e35fdac32080a5eb8960e04810c7
David Monk <d.g.monk0@gmail.com>
2016-11-04 21:37:17 +0000

    Updated git repository.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo 'Starting compilation...'
 gcc -fPIC -std=c99 -shared -o smallangle.so smallangle.c `pkg-config --cflags --libs gsl`
-gcc -fPIC -std=c99 -shared -o doublependulum.so doublependulum.c `pkg-config --cflags --libs gsl`
+gcc -fPIC -std=c99 -shared -o singlependulum.so singlependulum.c `pkg-config --cflags --libs gsl`
 echo 'Compiled succesfully. Starting plot.'
 python Main.py
